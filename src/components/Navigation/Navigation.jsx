549b6d023689d93266487dfbc02e74dce39f7f2b
Антон Ткаченко <antontkacenko@MacBook-Air-Anton.local>
2023-09-03 20:45:07 +0300

    final work maybe

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -11,16 +11,16 @@ import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
 import { NavLink } from "react-router-dom";
 import { MENU } from "../constants/constantns";
 import logoImage from "./logo.png";
 import "./Navigation.css";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createTheme, ThemeProvider, useMediaQuery } from "@mui/material";
 import { red } from "@mui/material/colors";
 import { useDispatch, useSelector } from "react-redux";
 import useRequest from "../../components/hooks/useRequest";
 import { setSearch } from "../store/SearchSlice";
+import Drawer from "@mui/material/Drawer"; // Импортируем компонент Drawer
 
 const pages = ["Home", "Films", "TV Show"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
@@ -38,10 +38,12 @@ const theme = createTheme({
 export default function Navigation() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false); // Состояние для открытия/закрытия бокового меню
 
   const dispatch = useDispatch();
   const apiSearch = useSelector((state) => state.search.value);
   const [inputValue, setInputValue] = React.useState("");
+  const isMobile = useMediaQuery("(max-width:600px)");
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -64,169 +66,168 @@ export default function Navigation() {
     dispatch(setSearch(e.target.value));
   };
 
+  const handleDrawerOpen = () => {
+    setIsDrawerOpen(true); // Открыть боковое меню
+  };
+
+  const handleDrawerClose = () => {
+    setIsDrawerOpen(false); // Закрыть боковое меню
+  };
+
   const apiData = useRequest(
     `https://api.tvmaze.com/search/shows?q=${apiSearch}`
   );
 
   return (
-    <AppBar
-      className="navigation-container"
-      sx={{
-        backgroundColor: "rgba(20, 20, 20, 0.7)",
-        position: "fixed",
-        zIndex: 100,
-        top: 0,
-      }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Avatar
-            alt="Logo"
-            src={logoImage}
-            sx={{
-              width: "250px",
-              height: "50px",
-            }}
-          />
+    <React.Fragment>
+      <AppBar
+        className="navigation-container"
+        sx={{
+          backgroundColor: "rgba(20, 20, 20, 0.7)",
+          position: "fixed",
+          zIndex: 100,
+          top: 0,
+        }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Avatar
+              alt="Logo"
+              src={logoImage}
+              sx={{
+                width: "250px",
+                height: "50px",
+              }}
+            />
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            {/* Добавляем кнопку для открытия бокового меню */}
             <IconButton
               size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
               color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
               sx={{
                 display: { xs: "block", md: "none" },
               }}
             >
-              {pages.map((page, index) => (
-                <MenuItem key={index} onClick={handleCloseNavMenu}>
-                  <Typography>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+              <MenuIcon />
+            </IconButton>
 
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              display: { xs: "none", md: "flex" },
-            }}
-          >
             <Box
-              className="navLinkContainer"
               sx={{
+                flexGrow: 1,
                 display: "flex",
+                justifyContent: "center",
                 alignItems: "center",
-                justifyContent: "space-between",
+                display: { xs: "none", md: "flex" },
               }}
             >
-              {MENU.map(({ name, link }, index) => (
-                <NavLink
-                  key={index}
-                  className={({ isActive }) =>
-                    `navLink ${isActive ? "isActive" : ""}`
-                  }
-                  to={link}
-                >
-                  {name}
-                </NavLink>
-              ))}
+              <Box
+                className="navLinkContainer"
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                {MENU.map(({ name, link }, index) => (
+                  <NavLink
+                    key={index}
+                    className={({ isActive }) =>
+                      `navLink ${isActive ? "isActive" : ""}`
+                    }
+                    to={link}
+                  >
+                    {name}
+                  </NavLink>
+                ))}
+              </Box>
             </Box>
-          </Box>
 
-          <ThemeProvider theme={theme}>
-            <TextField
-              label="Поиск..."
-              value={inputValue}
-              onChange={handleSearch}
-              sx={{
-                width: "20rem",
-                marginRight: "5rem",
-                "& input": {
-                  width: "100%",
-                  borderColor: theme.palette.primary.main,
-                  color: "#fff",
-                },
-                "& label": {
-                  color: "#fff",
-                },
-              }}
-            />
-          </ThemeProvider>
+            <ThemeProvider theme={theme}>
+              <TextField
+                label="Поиск..."
+                value={inputValue}
+                onChange={handleSearch}
+                sx={{
+                  width: "20rem",
+                  minWidth: "4rem",
+                  marginRight: "5rem",
+                  "& input": {
+                    width: "100%",
+                    borderColor: theme.palette.primary.main,
+                    color: "#fff",
+                  },
+                  "& label": {
+                    color: "#fff",
+                  },
+                }}
+              />
+            </ThemeProvider>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <Drawer
+        anchor="left"
+        open={isDrawerOpen}
+        onClose={handleDrawerClose}
+        sx={{
+          "& .MuiDrawer-paper": {
+            backgroundColor: "rgba(20, 20, 20, 0.7)",
+            color: "#fff",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            width: 250,
+            display: "flex",
+            flexDirection: "column",
+            paddingTop: 2,
+          }}
+        >
+          {pages.map((page, index) => (
+            <NavLink
+              key={index}
+              to={MENU[index].link}
+              className="drawerNavLink"
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              {page}
+            </NavLink>
+          ))}
+        </Box>
+      </Drawer>
+    </React.Fragment>
   );
 }
